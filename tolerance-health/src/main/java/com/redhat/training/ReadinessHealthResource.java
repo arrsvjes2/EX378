6b342e66b516492937580b51cfcb7d0feae9082e
arrsvjes2 <111775572+arrsvjes2@users.noreply.github.com>
2023-04-25 19:12:36 -0600

    Update ReadinessHealthResource.java

--- tolerance-health/src/main/java/com/redhat/training/ReadinessHealthResource.java
+++ tolerance-health/src/main/java/com/redhat/training/ReadinessHealthResource.java
@@ -10,4 +10,9 @@ import org.eclipse.microprofile.health.Readiness;
 public class ReadinessHealthResource {
 
     private final String HEALTH_CHECK_NAME = "Readiness";
+    public HealthCheckResponse call() {
+        return ++counter >= 10
+            ? HealthCheckResponse.up(HEALTH_CHECK_NAME)
+            : HealthCheckResponse.down(HEALTH_CHECK_NAME);
+    }
 }
